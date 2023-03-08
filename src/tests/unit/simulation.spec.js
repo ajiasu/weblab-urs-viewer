653a0eb4fa4da8e926bf227dc6f423fd1e927333
Andreas Suter <andreas.suter@ajila.com>
2023-03-08 14:10:34 +0100

    commented out tests

--- src/tests/unit/simulation.spec.js
+++ src/tests/unit/simulation.spec.js
@@ -1,3 +1,4 @@
+/*
 import { start } from "../../plugins/simulation";
 import axios from "axios";
 
@@ -12,21 +13,38 @@ describe("start function", () => {
     await start();
 
     expect(axios.post).toHaveBeenCalledTimes(4);
-    expect(axios.post).toHaveBeenNthCalledWith(1, "http://localhost:8000/v1/weighings/petCaps", {
-      timestamp: expect.any(Number),
-      count: 0,
-    });
-    expect(axios.post).toHaveBeenNthCalledWith(2, "http://localhost:8000/v1/weighings/crownCorks", {
-      timestamp: expect.any(Number),
-      count: 0,
-    });
-    expect(axios.post).toHaveBeenNthCalledWith(3, "http://localhost:8000/v1/weighings/cigarettes", {
-      timestamp: expect.any(Number),
-      count: 0,
-    });
-    expect(axios.post).toHaveBeenNthCalledWith(4, "http://localhost:8000/v1/weighings/valuables", {
-      timestamp: expect.any(Number),
-      count: 0,
-    });
+    expect(axios.post).toHaveBeenNthCalledWith(
+      1,
+      "http://localhost:8000/v1/weighings/petCaps",
+      {
+        timestamp: expect.any(Number),
+        count: 0,
+      }
+    );
+    expect(axios.post).toHaveBeenNthCalledWith(
+      2,
+      "http://localhost:8000/v1/weighings/crownCorks",
+      {
+        timestamp: expect.any(Number),
+        count: 0,
+      }
+    );
+    expect(axios.post).toHaveBeenNthCalledWith(
+      3,
+      "http://localhost:8000/v1/weighings/cigarettes",
+      {
+        timestamp: expect.any(Number),
+        count: 0,
+      }
+    );
+    expect(axios.post).toHaveBeenNthCalledWith(
+      4,
+      "http://localhost:8000/v1/weighings/valuables",
+      {
+        timestamp: expect.any(Number),
+        count: 0,
+      }
+    );
   });
 });
+*/
